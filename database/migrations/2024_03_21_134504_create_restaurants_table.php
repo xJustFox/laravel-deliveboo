df0e64561ac5534b7ae9d5b174685dd93b759332
xJustFox <dodydaversa@gmail.com>
2024-03-21 16:03:24 +0100

    Fix - aggiunto lo slug alla table restaurants

--- database/migrations/2024_03_21_134504_create_restaurants_table.php
+++ database/migrations/2024_03_21_134504_create_restaurants_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('name', 100);
+            $table->string('slug');
             $table->string('address', 100);
             $table->string('p_iva', 11);
             $table->string('main_image', 255);
