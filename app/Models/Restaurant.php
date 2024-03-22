0e1f0420e56a6fc683013e28ab4cd1956e3602f8
aletardio <ale.tardio99@gmail.com>
2024-03-22 11:17:44 +0100

    [Add] Seeder typologies restaurants

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,11 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'user_id',
         'name',
+        'slug',
         'address',
         'p_iva',
         'main_image',
     ];
+
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class);
+    }
 }
