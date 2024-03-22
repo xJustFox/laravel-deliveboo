bfbfec1466f95274175279184cbe7d5ae4a15fc1
xJustFox <dodydaversa@gmail.com>
2024-03-22 22:00:52 +0100

    [Upgrade] Customizzati i messaggi di errore nel form di registrazione

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,18 +34,52 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        $customMessages = [
+            'name.required' => 'Il campo Nome e Cognome è obbligatorio.',
+            'name.string' => 'Il campo Nome e Cognome deve essere una stringa.',
+            'name.max' => 'Il campo Nome e Cognome non può superare 255 caratteri.',
+        
+            'email.required' => 'Il campo Indirizzo E-Mail è obbligatorio.',
+            'email.string' => 'Il campo Indirizzo E-Mail deve essere una stringa.',
+            'email.email' => 'Il campo Indirizzo E-Mail deve essere un indirizzo email valido.',
+            'email.max' => 'Il campo Indirizzo E-Mail non può superare 255 caratteri.',
+            'email.unique' => 'Questa E-Mail è già stata utilizzata.',
+            
+            'password.required' => 'Il campo Password è obbligatorio.',
+            'password.confirmed' => 'La conferma della password non corrisponde.',
+            'password' => 'La Password deve avere almeno :min caratteri, includere almeno una lettera maiuscola, una lettera minuscola, un numero e un carattere speciale.',
+            
+            // Validazione dei dati inviati dall'utente
+            'restaurantName.required' => 'Il campo Nome Ristorante è obbligatorio.',
+            'restaurantName.string' => 'Il campo Nome Ristorante deve essere una stringa.',
+            'restaurantName.max' => 'Il campo Nome Ristorante non può superare 100 caratteri.',
+            
+            'address.required' => 'Il campo Indirizzo è obbligatorio.',
+            'address.string' => 'Il campo Indirizzo deve essere una stringa.',
+            'address.max' => 'Il campo Indirizzo non può superare 100 caratteri.',
+            
+            'p_iva.required' => 'Il campo Partita IVA è obbligatorio.',
+            'p_iva.string' => 'Il campo Partita IVA deve essere una stringa.',
+            'p_iva.max' => 'Il campo Partita IVA non può superare 11 caratteri.',
+            
+            'main_image.required' => 'Il campo Immagine di Copertina è obbligatorio.',
+            
+            'typology_id.required' => 'Il campo Tipologia Ristorante è obbligatorio.'
+        ];
+        
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-
+        
             // valido i dati inviati dall'utente
             'restaurantName' => ['required', 'string', 'max:100'],
             'address' => ['required', 'string', 'max:100'],
             'p_iva' => ['required', 'string', 'max:11'],
-            'main_image' => ['required', 'string'],
+            'main_image' => ['required'],
             'typology_id' => ['required']
-        ]);
+        ], $customMessages);
+        
 
         $user = User::create([
             'name' => $request->name,
