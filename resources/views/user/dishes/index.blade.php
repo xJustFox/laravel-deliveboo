4532caf6f148a671f150ce9867e1be93b3d7e271
xJustFox <dodydaversa@gmail.com>
2024-03-25 15:48:54 +0100

    [Upgrade] Modificato stile del Menù

--- resources/views/user/dishes/index.blade.php
+++ resources/views/user/dishes/index.blade.php
@@ -3,41 +3,72 @@
 @section('content')
     <div class="container" id="menu-page">
         <h2 class="fs-2 text-secondary my-4 text-white">
-            <div class="super-ocean">
-                {{ __('Menù') }}
-            </div>
-            <div class="d-flex justify-content-end">
-                <a class="btn btn-sm btn-warning add-dish" href="{{ route('user.dishes.create') }}">Aggiungi piatto</a>
+            <div class="d-flex justify-content-between">
+                <div class="super-ocean">
+                    {{ __('Menù') }}
+                </div>
+                <a class="btn add-dish" href="{{ route('user.dishes.create') }}">Aggiungi piatto</a>
             </div>
         </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card dishes">
-                    <div class="card-body">
-                        @foreach ($dishes as $dish)
-                            <div>Nome piatto: {{ $dish->name }}</div>
-                            <div>Genere: {{ $dish->genre->name }}</div>
-                            <div>Descrizone: {{ $dish->description }}</div>
-                            <div>Prezzo: {{ $dish->price }}$</div>
-                            <div><br> <img class=" w-25" src="{{ $dish->image }}" alt=""></div>
-                            <div class="text-end">
-                                <a class="btn btn-sm btn-warning"
-                                    href="{{ route('user.dishes.show', ['dish' => $dish->slug]) }}">Dettaglio</a>
-                                <a class="btn btn-sm btn-warning"
-                                    href="{{ route('user.dishes.edit', ['dish' => $dish->slug]) }}"><i
-                                        class="fa-solid fa-pen-to-square"></i></a>
-
-                                <button class="btn btn-sm btn-warning delete-button" data-bs-toggle="modal"
-                                    data-bs-target="#modal_delete" data-path="dishes" data-slug="{{ $dish->slug }}">
-                                    <i class="fa-solid fa-trash-can fa-xs"></i>
-                                </button>
+        @foreach ($genres as $genre)
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header text-center super-ocean">
+                            <h5>{{$genre->name}}</h5>
+                        </div>
+                        <div class="card-body">
+                            @foreach ($dishes as $dish)
+                            @if ($dish->genre->name == $genre->name)
+                            <div class="row">
+                                <div class="col-12 pb-4 d-flex justify-content-between align-items-center">
+                                    <div>
+                                        @if ($dish->visible == 1)
+                                            <div class="badge text-bg-success">
+                                                Disponibile
+                                            </div>
+                                        @else
+                                            <div class="badge text-bg-danger">
+                                                Non Disponibile
+                                            </div>
+                                        @endif
+                                    </div>
+                                    <div>
+                                        <a class="btn"
+                                            href="{{ route('user.dishes.edit', ['dish' => $dish->slug]) }}"><i
+                                                class="fa-solid fa-pen-to-square"></i></a>
+        
+                                        <button class="btn delete-button" data-bs-toggle="modal"
+                                            data-bs-target="#modal_delete" data-path="dishes" data-slug="{{ $dish->slug }}">
+                                            <i class="fa-solid fa-trash-can fa-xs"></i>
+                                        </button>
+                                    </div>
+                                </div>
+    
+                                <div class="col-8">
+                                    <div class="row">
+                                        <div class="col-6 row"> 
+                                            <div class="col-12"><div class="super-ocean fs-5">Nome piatto:</div> {{ $dish->name }}</div>
+                                            <div class="col-12"> <div class="super-ocean fs-5">Prezzo:</div> {{ $dish->price }}$</div>
+                                            <div class="col-12"> <div class="super-ocean fs-5">Genere:</div> {{ $dish->genre->name }}</div>
+                                        </div>
+                                        <div class="col-6"> <div class="super-ocean fs-5">Descrizone:</div> {{ $dish->description }}</div>
+                                    </div>
+                                </div>
+    
+                                <div class="col-4">
+                                    <img class="w-100" src="{{ $dish->image }}" alt="">
+                                </div>
                             </div>
+
                             <hr>
-                        @endforeach
+                            @endif
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
     @include('partials.modal_delete')
 @endsection
