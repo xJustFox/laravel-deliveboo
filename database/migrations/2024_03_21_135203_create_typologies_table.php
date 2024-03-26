cc101229d3717a3eb6069a7338fb97cd711cc895
xJustFox <dodydaversa@gmail.com>
2024-03-26 14:48:01 +0100

    [Upgrade] Aggiornata migration e seeder di typology

--- database/migrations/2024_03_21_135203_create_typologies_table.php
+++ database/migrations/2024_03_21_135203_create_typologies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('typologies', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
+            $table->string('slug');
             $table->timestamps();
         });
     }
