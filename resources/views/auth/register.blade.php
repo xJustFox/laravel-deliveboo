b2deea8ade6bc3160abb80ee5c7b9b39d028baaf
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-26 11:42:30 +0100

    [Fix] Aggiunto * per segnare i campi obbligatori

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+
+    {{-- Pagina di registrazione --}}
     <div class="container mt-4" id="register-page">
         <div class="row justify-content-center">
             <div class="col-8">
                 <div class="card ">
+
+                    {{-- Header form registrazione --}}
                     <div class="card-header text-white text-center super-ocean">{{ __('Register') }}</div>
 
+                    {{-- Body form registrazione --}}
                     <div class="card-body">
                         <form class="row" method="POST" action="{{ route('register') }}">
                             @csrf
 
+                            {{-- Sezione nome e cognome --}}
                             <div class="col-md-6 mb-2">
-                                <label for="name" class="col-form-label text-white">{{ __('Nome e Cognome') }}</label>
+                                <label for="name" class="col-form-label text-white">{{ __('Nome e Cognome *') }}</label>
                                 <input id="name" type="text"
                                     class="form-control form-control-sm my-form @error('name') is-invalid @enderror"
                                     name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -24,8 +30,9 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione email --}}
                             <div class="col-md-6 mb-2">
-                                <label for="email" class="col-form-label text-white">{{ __('Indirizzo E-Mail') }}</label>
+                                <label for="email" class="col-form-label text-white">{{ __('Indirizzo E-Mail *') }}</label>
                                 <input id="email" type="email"
                                     class="form-control form-control-sm my-form @error('email') is-invalid @enderror"
                                     name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -37,8 +44,9 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione password --}}
                             <div class="col-md-6 mb-2">
-                                <label for="password" class="col-form-label text-white">{{ __('Password') }}</label>
+                                <label for="password" class="col-form-label text-white">{{ __('Password *') }}</label>
                                 <input id="password" type="password"
                                     class="form-control form-control-sm my-form @error('password') is-invalid @enderror"
                                     name="password" required autocomplete="new-password">
@@ -50,18 +58,19 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione conferma password --}}
                             <div class="col-md-6 mb-2">
                                 <label for="password-confirm"
-                                    class="col-form-label text-white">{{ __('Conferma Password') }}</label>
+                                    class="col-form-label text-white">{{ __('Conferma Password *') }}</label>
                                 <input id="password-confirm" type="password" class="form-control form-control-sm my-form"
                                     name="password_confirmation" required autocomplete="new-password">
                             </div>
 
-                            {{-- restaurants  side --}}
-
+                            {{-- Restaurants  side --}}
+                            {{-- Sezione nome ristorante --}}
                             <div class="col-md-6 mb-2">
                                 <label for="restaurantName"
-                                class="col-form-label text-white">{{ __('Nome Ristorante') }}</label>
+                                class="col-form-label text-white">{{ __('Nome Ristorante *') }}</label>
                                 <input id="restaurantName" type="text"
                                 class="form-control form-control-sm my-form @error('restaurantName') is-invalid @enderror"
                                 name="restaurantName" value="{{ old('restaurantName') }}" required
@@ -74,8 +83,9 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione tipologia ristorante --}}
                             <div class="col-md-6 mb-2">
-                                <label for="typology_id" class="col-form-label text-white">Tipologia Ristorante:</label>
+                                <label for="typology_id" class="col-form-label text-white">Tipologia Ristorante: *</label>
                                 <div class="btn-group w-100" id="typology_id">
                                     <button class="form-control form-control-sm my-form d-flex justify-content-between align-items-center @error('typologies') is-invalid @enderror" type="button" data-bs-toggle="dropdown" data-bs-auto-close="false" aria-expanded="false">
                                       Scegli una tipologia...
@@ -99,8 +109,9 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione indirizzo --}}
                             <div class="col-md-6 mb-2">
-                                <label for="address" class="col-form-label text-white">{{ __('Indirizzo') }}</label>
+                                <label for="address" class="col-form-label text-white">{{ __('Indirizzo *') }}</label>
                                 <input id="address" type="text"
                                     class="form-control form-control-sm my-form @error('address') is-invalid @enderror"
                                     name="address" value="{{ old('address') }}" required autocomplete="address">
@@ -112,8 +123,9 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione p.iva --}}
                             <div class="col-md-6 mb-2">
-                                <label for="p_iva" class="col-form-label text-white">{{ __('Partita IVA') }}</label>
+                                <label for="p_iva" class="col-form-label text-white">{{ __('Partita IVA *') }}</label>
                                 <input id="p_iva" type="text"
                                     class="form-control form-control-sm my-form @error('p_iva') is-invalid @enderror"
                                     name="p_iva" value="{{ old('p_iva') }}" required autocomplete="p_iva">
@@ -125,9 +137,10 @@
                                 @enderror
                             </div>
 
+                            {{-- Sezione immagine di copertina --}}
                             <div class="col-md-12 mb-3">
                                 <label for="main_image"
-                                    class="col-form-label text-white">{{ __('Immagine di copertina') }}</label>
+                                    class="col-form-label text-white">{{ __('Immagine di copertina *') }}</label>
                                 <input id="main_image" type="text"
                                     class="form-control form-control-sm my-form @error('main_image') is-invalid @enderror"
                                     name="main_image" value="{{ old('main_image') }}" required autocomplete="main_image">
@@ -139,12 +152,18 @@
                                 @enderror
                             </div>
 
+                            {{-- Campi obbligatori --}}
+                            <div class="col-md-8 col-xs-12 d-flex align-items-end text-light py-2 ">
+                                <span class="fst-italic fw-light text-decoration-underline req_fields">{{__('Sono contrassegnati con * i campi obbligatori')}}</span>
+                            </div>
 
-                            <div class="col-12 d-flex justify-content-end">
+                            {{-- Pulsante registrazione --}}
+                            <div class="col-md-4 col-xs-12 d-flex justify-content-end py-2 ">
                                 <button type="submit" class="btn btn-sm">
                                     {{ __('Register') }}
                                 </button>
                             </div>
+                            
                         </form>
                     </div>
                 </div>
--- resources/views/user/dishes/edit.blade.php
+++ resources/views/user/dishes/edit.blade.php
@@ -15,9 +15,6 @@
                         <span>
                             Modifica piatto
                         </span>
-                        <span class="arrow-rotate">
-                            <a  href="{{ route('user.dishes.index') }}"><i class="fa-solid fa-arrow-rotate-left" style="color: #DA643F;"></i></a>
-                        </span>
                     </div>
 
                     {{-- Card Body --}}
@@ -30,7 +27,7 @@
 
                                     {{-- Sezione label per la modifica del nome --}}
                                     <div class="col-12 mb-2">
-                                        <label for="name" class="form-label text-white">Nome:</label>
+                                        <label for="name" class="form-label text-white">Nome: *</label>
                                         <input name="name" type="text" class="form-control form-control-sm my-form"
                                             id="name" placeholder="Inserisci il nome del piatto..."
                                             value="{{ $dish->name }}" required>
@@ -42,7 +39,7 @@
                                     
                                     {{-- Sezione label per la modifica del genere--}}
                                     <div class="col-12 mb-2">
-                                        <label for="genre_id" class="form-label text-white">Genere:</label>
+                                        <label for="genre_id" class="form-label text-white">Genere: *</label>
                                         <div class="input-group">
                                             <select class="form-select form-select-sm my-select" name="genre_id" id="genre_id" value="{{ $dish->genre_id }}" required>
                                                 <option class="my-option" value="">Imposta il tipo di piatto...</option>
@@ -58,7 +55,7 @@
 
                                     {{-- Sezione label per la modifica del prezzo --}}
                                     <div class="col-12 mb-2">
-                                        <label for="price" class="form-label text-white">Prezzo:</label>
+                                        <label for="price" class="form-label text-white">Prezzo: *</label>
                                         <div class="input-group input-group-sm ">
                                             <span class="input-group-text my-input-text">€</span>
                                             <input name="price" type="number" class="form-control form-control-sm my-form"
@@ -72,7 +69,7 @@
                                     
                                     {{-- Sezione label per la modifica della disponibilità --}}
                                     <div class="col-12 mb-2">
-                                        <label for="visible" class="form-label text-white">Disponibilità:</label>
+                                        <label for="visible" class="form-label text-white">Disponibilità: *</label>
                                         <div class="input-group">
                                             <select class="form-select form-select-sm my-select" name="visible" id="visible" required>
                                                 <option class="my-option" value="" selected>Seleziona la disponibilità...</option>
@@ -87,21 +84,26 @@
                                     
                                     {{-- Sezione label per la modifica immagine --}}
                                     <div class="col-12 mb-2">
-                                        <label for="image" class="form-label text-white">Immagine:</label>
+                                        <label for="image" class="form-label text-white">Immagine: *</label>
                                         <input type="text" name="image" id="image" class="form-control form-control-sm my-form" value="{{ $dish->image }}">
                                     </div>
                                     
                                     {{-- Sezione label per la modifica della descrizione --}}
                                     <div class="col-12 mb-2">
-                                        <label for="description" class="form-label text-white">Descrizione:</label>
+                                        <label for="description" class="form-label text-white">Descrizione: *</label>
                                         <textarea name="description" class="form-control form-control-sm my-form" placeholder="Aggiungi una descrizione..." id="description" style="height: 100px" required>{{ $dish->description }}</textarea>
                                         @error('description')
                                             <div class="text-danger">{{ $message }}</div>
                                         @enderror
                                     </div>
 
+                                    {{-- Campi obbligatori --}}
+                                    <div class="col-md-8 col-xs-12 d-flex align-items-end text-light py-2 ">
+                                        <span class="fst-italic fw-light text-decoration-underline req_fields">{{__('Sono contrassegnati con * i campi obbligatori')}}</span>
+                                    </div>
+
                                     {{-- Pulsante submit --}}
-                                    <div class="col-12 text-center mt-4">
+                                    <div class="text-center col-md-4 col-xs-12 d-flex justify-content-end py-2">
                                         <button type="submit" value="Submit" class="btn btn-sm btn-primary float-end">Aggiungi</button>
                                     </div>
                                 </form>
