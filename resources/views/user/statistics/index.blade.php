8b04ee6654fd4b15d8469e83a93c8a6f751cc9d5
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-04-05 16:16:20 +0200

    [Add] Aggiunto secondo grafico "Ordini ricevuti"

--- resources/views/user/statistics/index.blade.php
+++ resources/views/user/statistics/index.blade.php
@@ -9,11 +9,17 @@
                 <h3>Incassi degli ultimi sei mesi</h3>
                 <canvas id="line-chart"></canvas>
             </div>
+
+            <div class="col-12 text-white my-5">
+                <h3 class="my-5">Ordini ricevuti negli ultimi sei mesi</h3>
+                <canvas id="orders-line-chart"></canvas>
+            </div>
         </div>
     </div>
 
     {{-- Include il codice JavaScript --}}
     <script>
+            // PRIMO GRAFICO
             // Recupera i dati delle ordinazioni dal server
             let orders = {!! json_encode($orders) !!};
 
@@ -72,5 +78,86 @@
                     }
                 }
             });
+            // SECONDO GRAFICO
+            const ordersLine = document.getElementById('orders-line-chart');
+            lastSixMonths = [];
+            for (let i = 0; i < 6; i++) {
+                lastSixMonths.push(new Date(new Date().setMonth(new Date().getMonth() - i)).getMonth() + 1)
+            }
+
+            let ordersInLastSixMonth = [0, 0, 0, 0, 0, 0]
+
+            orders.forEach(order => {
+                let month = new Date(order.created_at).getMonth() + 1
+                lastSixMonths.indexOf(month) !== -1 ? ordersInLastSixMonth[lastSixMonths.indexOf(month)] += 1 : null
+            })
+
+            lastSixMonths.forEach((month, i) => {
+            switch (month) {
+                    case 1:
+                        lastSixMonths[i] = 'Gennaio';
+                        break;
+                    case 2:
+                        lastSixMonths[i] = 'Febbraio';
+                        break;
+                    case 3:
+                        lastSixMonths[i] = 'Marzo';
+                        break;
+                    case 4:
+                        lastSixMonths[i] = 'Aprile';
+                        break;
+                    case 5:
+                        lastSixMonths[i] = 'Maggio';
+                        break;
+                    case 6:
+                        lastSixMonths[i] = 'Giugno';
+                        break;
+                    case 7:
+                        lastSixMonths[i] = 'Luglio';
+                        break;
+                    case 8:
+                        lastSixMonths[i] = 'Agosto';
+                        break;
+                    case 9:
+                        lastSixMonths[i] = 'Settembre';
+                        break;
+                    case 10:
+                        lastSixMonths[i] = 'Ottobre';
+                        break;
+                    case 11:
+                        lastSixMonths[i] = 'Novembre';
+                        break;
+                    case 12:
+                        lastSixMonths[i] = 'Dicembre';
+                        break;
+                    default:
+                        break;
+                }
+            })
+
+            lastSixMonths = lastSixMonths.reverse()
+            ordersInLastSixMonth = ordersInLastSixMonth.reverse()
+
+            new Chart(ordersLine, {
+            type: 'line',
+            data: {
+                labels: lastSixMonths,
+                datasets: [{
+                    label: 'Ordini',
+                    data: ordersInLastSixMonth,
+                    borderWidth: 1,
+                    borderColor: 'rgba(142, 250, 246, 1)',
+                    backgroundColor: 'rgba(142, 250, 246, 1)'
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
     </script>
 @endsection
