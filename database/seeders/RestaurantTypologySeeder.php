e521746d609fa94c551c584f59279625b27c2e01
xJustFox <dodydaversa@gmail.com>
2024-04-09 11:34:41 +0200

    [FIx] Modifcato seeder typologies

--- database/seeders/RestaurantTypologySeeder.php
+++ database/seeders/RestaurantTypologySeeder.php
@@ -16,13 +16,18 @@ class RestaurantTypologySeeder extends Seeder
             ['restaurant_id' => 1, 'typology_id' => 10], // Mc Donalds -> Fast food
             ['restaurant_id' => 2, 'typology_id' => 10], // Burger King -> Fast food
             ['restaurant_id' => 3, 'typology_id' => 5],  // Sushi Li -> Ristorante di sushi
+            ['restaurant_id' => 3, 'typology_id' => 7],
             ['restaurant_id' => 4, 'typology_id' => 1],  // Pizzeria Gino Sorbillo -> Ristorante italiano
+            ['restaurant_id' => 4, 'typology_id' => 11],
             ['restaurant_id' => 5, 'typology_id' => 1],  // Pizza al trancio -> Ristorante italiano
             ['restaurant_id' => 6, 'typology_id' => 7],  // Ristorante Migasa -> Ristorante di pesce
+            ['restaurant_id' => 6, 'typology_id' => 12],
             ['restaurant_id' => 7, 'typology_id' => 6],  // Rock 'N Roll -> Ristorante di barbecue
+            ['restaurant_id' => 7, 'typology_id' => 13],
             ['restaurant_id' => 8, 'typology_id' => 10], // Ristorante Piedra del Sol -> Fast food
             ['restaurant_id' => 9, 'typology_id' => 9],  // Ristorante Maya -> Ristorante etnico
             ['restaurant_id' => 10, 'typology_id' => 12],// Ristorante Miao Chinese -> Ristorante fusion
+            ['restaurant_id' => 10, 'typology_id' => 9],
         ];
 
         foreach ($restaurantTypologies as $rt) {
